eee4ca5ede2c3f58b5df5a684910e837550e1de2
Orcanshul <Anshul0402@gmail.com>
2026-02-14 22:04:31 -0800

    added main class and worked with the WordFreq dataset

---
+++ MaxHeapBuilder.java
@@ -0,0 +1,27 @@
+public class MaxHeapBuilder {
+
+    public static void main(String[] args) {
+
+        WordFreq[] arr = {
+            new WordFreq("happy", 400),
+            new WordFreq("satisfied", 100),
+            new WordFreq("neutral", 300),
+            new WordFreq("would buy again", 200),
+            new WordFreq("terrible", 160),
+            new WordFreq("inconvenient", 900),
+            new WordFreq("difficult to use", 100),
+            new WordFreq("easy to use", 140),
+            new WordFreq("would recommend to friends", 800),
+            new WordFreq("visit the store", 700)
+        };
+
+        System.out.println("Original array:");
+        printArray(arr);
+    }
+
+    static void printArray(WordFreq[] arr) {
+        for (WordFreq wf : arr) {
+            System.out.println(wf);
+        }
+    }
+}
